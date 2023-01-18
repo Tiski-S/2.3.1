5063f2dd14143e0ce7da7fa2ded8038d92346e82
Andrei Kryvaltsevich <krays170@gmail.com>
2023-01-18 04:43:21 +0300

    Fix

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -12,43 +12,42 @@ import web.service.UserService;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
-//working
-    @RequestMapping(method = RequestMethod.GET)
+
+    @GetMapping()
     public String printUsers(ModelMap model) {
         model.addAttribute("users", userService.getUsers());
         return "users";
     }
 
-//working
+
     @GetMapping("/new")
     public String newUser(@ModelAttribute("users") User user) {
         return "new";
     }
-//working
+
     @PostMapping()
     public String createUser(@ModelAttribute("users") User user) {
         userService.addUser(user);
         return "redirect:/users";
     }
-//working
+
     @GetMapping("/{id}/edit")
     public String editUser(@PathVariable("id") int id, Model model) {
         model.addAttribute("users", userService.getUserById(id));
         return "edit";
     }
-//working
+
     @PatchMapping("/{id}")
     public String updateUser(@ModelAttribute("users") User user, @PathVariable("id") int id) {
         userService.update(id,user);
         return "redirect:/users";
     }
 
-//working
-@DeleteMapping("/{id}")
+
+    @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable("id") int id) {
         userService.delete(id);
         return "redirect:/users";
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Service;
 import web.dao.UserDao;
 import web.model.User;
 
+import javax.transaction.Transactional;
 import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService{
 
@@ -14,25 +16,30 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
+    @Transactional
     public void addUser(User user) {
         userDao.addUser(user);
     }
 
     @Override
+    @Transactional
     public List<User> getUsers() {
         return userDao.getUsers();
     }
 
     @Override
+    @Transactional
     public void update(int id, User updateUser) {
         userDao.update(id, updateUser);
     }
 
     @Override
+    @Transactional
     public void delete(int id) {
         userDao.delete(id);
     }
     @Override
+    @Transactional
     public User getUserById(int id) {
         return userDao.getUserById(id);
     }
